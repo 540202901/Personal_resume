9d4101b932261b26ad2b4bd425a84d6820369f17
Kai Wang <540202901@qq.com>
2019-04-23 14:30:30 +0800

    制作主页面

--- src/app.vue
+++ src/app.vue
@@ -1,161 +1,271 @@
 <template>
-    <div class="app-container">
+  <div class="app-container" :class="appFlag ? 'appMain':''">
+    <div class="content" :class="[toMainTranFlag ? 'toMainTransition':'',appFlag ? 'mainBox':'']">
 
+      <!--头像部分-->
+      <header @click="goMain"  >
+        <headerBox  :hidden-flag="toMainTranFlag"  :app-flag="appFlag"></headerBox>
+      </header>
+      <!--头像部分-->
 
-      <div class="beginPage" >
-        <img class="front" src="./images/front-image.jpg" alt="">
-        <div class="name"><H1>{{ name }}</H1></div>
-        <div class="skill">
-          Web
-          <transition enter-active-class="animated flipInX" leave-active-class="animated flipOutX">
-            <span class="rotate ">{{ rotateSkill[0] }}</span>
-          </transition>
+      <!--侧边导航栏-->
+      <transition enter-active-class="animated fadeInUp">
+        <nav v-if="appFlag">
+          <navigation></navigation>
+        </nav>
+      </transition>
+      <!--侧边导航栏-->
+
+      <!--内容头部-->
+      <transition enter-active-class="animated fadeInDown">
+        <div class='main-header' v-if="MHeaderFlag">
         </div>
-        <div class="mouse">
-          <img src="./images/click.png" alt="">
-          <span class="pulse">
-          </span>
-        </div>
-      </div>
+      </transition>
+      <!--内容头部-->
 
+      <!--内容面板-->
+      <transition enter-active-class="animated fadeInUp">
+        <main v-if="mainFlag">
+        </main>
+      </transition>
+      <!--内容面板-->
 
-
+      <!--页脚-->
+      <transition enter-active-class="animated fadeInUp">
+        <footer v-if="appFlag">
+        </footer>
+      </transition>
+      <!--页脚-->
     </div>
+
+
+
+
+
+
+
+  </div>
 </template>
 
 <script>
+  import headerBox from './components/Header.vue'
+  import navigation from './components/Navigation.vue'
+
   export default {
-    name: "app",
+    name:'app',
     data:function(){
       return {
-        name:'王浩然',
-        rotateSkill:["设计师","学习者","开发者"],
+        //是否进入了主页
+        appFlag:false,
+        toMainTranFlag:false,//主页过渡样式是否打开
+        //其他盒子显示
+        MHeaderFlag:false,
+        mainFlag:false,
       }
     },
     mounted:function(){//挂载结束后生命周期钩子
       this.loading()
-    },
+    },//挂载结束后生命周期钩子
     methods:{
       loading: function(){
         //关闭等待页面
         var loadBox = window.document.getElementById("loading-screen");
-        setInterval(function(){
+        setTimeout(function(){
           loadBox.classList.add("animated","fadeOut")
-        },100)//等待100毫秒再加消失类,以免看到渲染过程
-        setInterval(function(){
+        },500)//等待100毫秒再加消失类,以免看到渲染过程
+        setTimeout(function(){
           loadBox.classList.add("none")
-        },1000)//等待100毫秒再加消失类,以免看到渲染过程
-      }//loading页面加载完成消失
+        },1000)//1秒后让盒子消失，避免遮挡内容的点击。
+      },//loading页面加载完成消失
+      //进入主页
+      goMain:function(){
+        this.toMainTranFlag = true//进入主页过渡动画
+        setTimeout(()=>{this.appFlag = true },1000)//进入主页
+        setTimeout(()=>{this.MHeaderFlag = true},1500)//主信息头部
+        setTimeout(()=>{this.mainFlag = true},2000)//主信息页面
+      },//进入主页
+    },
+    components:{
+      headerBox,//头像盒子组件
+      navigation,//导航组件
+    },
 
-
-    }
 
 
   }
 </script>
 
 <style lang="scss" scoped>
-    .app-container{
-      @import './font/font.css';//引入字体
+  /*首页样式*/
+  .app-container{
+    /*app根界面*/
+    @import './font/font.css';//引入字体
+    background: url('./images/main-bg.jpg') bottom no-repeat;
+    background-size:cover;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-wrap:wrap;
+    justify-content: center;
+    align-items: center;
 
+    overflow-x:hidden;
+    /*app根界面*/
 
-
-      background: url('./images/main-bg.jpg') bottom no-repeat;
-      background-size:cover;
-      width: 100%;
-      height: 100%;
-
+    /*容器盒子宽高100%，内容居中*/
+    .content{
+      width: 800px;
+      height: 600px;
       display: flex;
+      flex-wrap:wrap;
       justify-content: center;
       align-items: center;
+      position: relative;
+    }
+    /*容器盒子宽高100%，内容居中*/
 
-      .loading-screen{
-        position:fixed;
-        bottom: 0;
-        margin: 0;
+    /*手机端*/
+    @media screen and (max-width:767px){
+      /*容器*/
+      .content{
+        margin: 15px;
+        height: auto;
+      }
+      /*容器*/
+    }   /*手机端*/
+  }
+  /*首页样式*/
+
+
+
+
+
+  /*过渡样式*/
+  .toMainTransition{
+    /*手机端布局:打开主页面后的样式*/
+    @media screen and (max-width:767px){
+      header{
+        width: 100%;
+      }
+    }
+    /*手机端布局:打开主页面后的样式*/
+
+  }
+  /*过渡样式*/
+
+
+
+  /*打开主页面后的样式*/
+  .app-container{
+    overflow: hidden;
+    /*隐藏*/
+    .none{
+      display: none;
+    }
+    /*隐藏*/
+
+    /*打开主页面后的样式*/
+    .mainBox{
+      display:grid;
+      grid-gap:15px;
+      grid-template-columns: repeat(12, 1fr);
+      grid-template-rows: 130px 170px 300px ;
+      grid-template-areas:
+          "h h h h c c c c c c c c "
+          "h h h h m m m m m m m m "
+          "n n n n m m m m m m m m ";
+      header,nav,.main-header,main,footer{
         width: 100%;
         height: 100%;
-        background: url('./images/Preloader_7.gif') center no-repeat rgba(246,215,109,1);
-        z-index: 9999;
+      }
+      /*头部部分*/
+      header{
+        grid-area:h;
+      }
+      /*头部部分*/
 
-
-      }/*等待页面*/
-
-      .beginPage{
-        display: block;
-        background: #0f0f0f;
-        padding: 15px;
-        vertical-align:middle;
-        color:white;
-        cursor:pointer;
-
-        .front{
-          width: 520px;
-          height: auto;
-        }
-        text-align:right;
-        /*名字字体*/
-
-        .name{
-          margin-top: 15px;
-
-          h1{
-            font-family:"zh35-yh",sans-serif;
-            font-weight: 400;
-            color:rgba(246,215,109,1);
-          }
-        }
-        .skill{
-          font-family:"zh35-yh",sans-serif;
-          margin-bottom: 10px;
-          font-size: 20px;
-        }
-
-
-        /*鼠标*/
-        position: relative;
-        .mouse{
-          position: absolute;
-          left: 48%;
-          bottom: 10px;
-          img{
-            width: 40px;
-            height: auto;
-          }
-          .pulse{
-          }
-
-          /*鼠标的点击动画*/
-          .pulse::after{
-            content: "";
-
-            display: block;
-            position: absolute;
-            top: 16px;
-            left: 5px;
-            height: 8px;
-            width: 15px;
-
-            border-radius: 50%;
-            box-shadow:0 0 1px 1px #bdbdbd;
-
-            animation:pulse 0.7s infinite linear;
-          }
-          @keyframes pulse{
-            form{
-              transform:scale(1) ;
-              opacity:1;
-
-            }
-            to{
-              transform:scale(3) ;
-              opacity:0;
-            }
-          }
-        }
-
-      }/*点击开始页面*/
+      nav{
+        background-color: yellow;
+        grid-area:n;
+      }
+      .main-header{
+        background-color: green;
+        grid-area:c;
+      }
+      main{
+        background-color: red;
+        grid-area:m;
+      }
+      footer{
+        display: none;
+        background-color: hotpink;
+      }
 
 
     }
+    /*打开主页面后的样式*/
+  }
+
+
+
+  /*手机端布局:打开主页面后的样式*/
+  @media screen and (max-width:767px){
+    .app-container.appMain{
+      overflow-x: hidden;
+      overflow-y: auto;
+      justify-content:flex-start;
+      align-items:flex-start;
+
+
+      /*主页样式*/
+      .mainBox {
+        overflow-y: auto;
+        grid-template-columns: repeat(2, 1fr);
+        grid-template-rows: 300px 60px 200px 300px 50px;
+        grid-template-areas:
+            "h h"
+            "n n"
+            "c c"
+            "m m"
+            "f f";
+
+        margin: 15px;
+        header,nav,.main-header,main,footer{
+          -webkit-box-sizing:border-box;
+          box-sizing:border-box;
+          margin: 0;
+          height: 100%;
+          width: 100%;
+        }
+
+        /*头部部分*/
+        header{
+        }
+        /*头部部分*/
+
+        nav{
+
+        }
+        .main-header{
+
+        }
+        main{
+
+        }
+        footer{
+          grid-area:f;
+          display: block;
+        }
+
+
+
+
+
+      }
+      /*主页样式*/
+    }
+  }
+  /*手机端布局:打开主页面后的样式*/
+  /*打开主页面后的样式*/
+
 </style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <b-list-group class="warningList" >
+      <b-list-group-item href="#" class="warning">Warning list group item</b-list-group-item>
+      <b-list-group-item href="#" class="warning">Warning list group item</b-list-group-item>
+      <b-list-group-item href="#" class="warning">Warning list group item</b-list-group-item>
+      <b-list-group-item href="#" class="warning">Warning list group item</b-list-group-item>
+    </b-list-group>
+    <!--页脚-->
+    <footer>
+    </footer>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Navigation",
+    data:function () {
+      return {
+
+      }
+    },
+    methods:{
+
+    },
+    components:{
+
+    }
+
+
+  }
+</script>
+
+<style lang="scss" scoped>
+  /*左侧导航栏*/
+  .warning{
+    background-color: #f4d03f;
+    border: 0 !important;
+    border-bottom: 2px solid #e1c03a !important;
+  }
+  .list-group-item:first-child{
+    border-top-left-radius: 0 !important;
+    border-top-right-radius: 0 !important;
+  }
+  .list-group-item:last-child{
+    border-bottom-left-radius: 0 !important;
+    border-bottom-right-radius: 0 !important;
+  }
+  /*左侧导航栏*/
+
+
+
+
+  /*手机端*/
+  @media screen and (max-width:767px){
+
+  }
+  /*手机端*/
+</style>
